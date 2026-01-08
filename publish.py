2f6e276760373a233c52518b5be64b14427fe899
aes2biz <aes2biz@gmail.com>
2026-01-09 02:49:52 +0500

    Refactor TelegramClient to use StringSession
    Updated TelegramClient initialization to use StringSession.

--- publish.py
+++ publish.py
@@ -1,15 +1,17 @@
-from telethon import TelegramClient
 import os
+from telethon import TelegramClient
+from telethon.sessions import StringSession
 
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
-SESSION = os.environ.get("TELEGRAM_SESSION")
-CHANNEL  = os.environ.get("CHANNEL")
+SESSION_STR = os.environ.get("TELEGRAM_SESSION")
+CHANNEL  = os.environ.get("TELEGRAM_CHANNEL")
 
 text = os.environ.get("PUBLISH_TEXT")
 image_url = os.environ.get("PICTURE_URL")
 
-client = TelegramClient(SESSION, API_ID, API_HASH)
+# ⚠ Используем StringSession
+client = TelegramClient(StringSession(SESSION_STR), API_ID, API_HASH)
 
 async def main():
     await client.start()
