49203a9e20067426616313497d0de9062e1e6e84
aes2biz <aes2biz@gmail.com>
2026-01-10 20:44:40 +0500

    Strip whitespace from image URL
    Trim whitespace from image URL if provided.

--- publish.py
+++ publish.py
@@ -11,15 +11,13 @@ client = TelegramClient(StringSession(SESSION_STR), API_ID, API_HASH)
 
 async def main():
     await client.start()
-
     # force dialogs load (optional, но помогает)
     await client.get_dialogs()
-
     text = os.environ.get("PUBLISH_TEXT")
     image_url = os.environ.get("PICTURE_URL")
-
+    if image_url:
+        image_url = image_url.strip()  # Remove leading/trailing whitespace
     channel_entity = await client.get_entity(CHANNEL)
-
     if image_url:
         await client.send_file(channel_entity, image_url, caption=text, parse_mode="html")
     else:
